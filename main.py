e591e998057b8f23fbd0091b676004ed4aa45883
stefansdog <stefan.landwehr1000@gmail.com>
2026-05-22 20:28:00 +0200

    Add CustomPath

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from tkinter import filedialog
 window = Tk()
 window.geometry("400x400")
 
+class CustomPath:
+    Windows64Media_loc =            StringVar(window, "D:/Games/MinecraftConsoles/Minecraft.Client/Windows64Media/loc")
+    Windows64Media =                StringVar(window, "D:/Games/MinecraftConsoles/Minecraft.Client/Windows64Media")
+    Common_Media =                  StringVar(window, "D:/Games/MinecraftConsoles/Minecraft.Client/Common/Media/")
+
 window.mainloop()

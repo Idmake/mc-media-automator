2f92da434a9aa1db8279a901ff1a4bfc353c0d8b
stefansdog <stefan.landwehr1000@gmail.com>
2026-05-22 20:50:21 +0200

    Implement new Pathselector features

--- main.py
+++ main.py
@@ -10,12 +10,31 @@ class CustomPath:
     Common_Media =                  StringVar(window, "D:/Games/MinecraftConsoles/Minecraft.Client/Common/Media/")
 
 class PathSelector:
-    def __init__(self, label_text):
-        Label(window, text=label_text).pack()
+    def __init__(self, description, pathvariable):
+        
+        Label(window,   text=           description).pack(pady=10)
+        Entry(window,   textvariable=   pathvariable).pack()
+        Button(window,  text=           "browse",   command=lambda: self.set_pathvariable(      pathvariable=pathvariable)).pack()
+        Button(window,  text=           "clear",    command=lambda: self.clear_pathvariable(    pathvariable=pathvariable)).pack()
 
-test1 = PathSelector("hi")
-test2 = PathSelector("this")
-test3 = PathSelector("is")
-test4 = PathSelector("cool")
+    # Clear pathvariable associated with this instance
+    def clear_pathvariable(self, pathvariable):
+        StringVar.set(pathvariable, "")
+
+    # Select a new path for the pathvariable
+    def set_pathvariable(self, pathvariable):
+        selected_path = filedialog.askopenfilename()
+
+        # We didn't select anything, return
+        if selected_path == "":
+            return
+        
+        # We selected a path, set it.
+        StringVar.set(pathvariable, selected_path)
+        
+
+
+test1 = PathSelector(description="hi",      pathvariable=CustomPath.Windows64Media_loc)
+test2 = PathSelector(description="bye",     pathvariable=CustomPath.Windows64Media)
 
 window.mainloop()

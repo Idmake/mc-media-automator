52e37ee69fb9b1770996f5a37d27844d1cd9c163
stefansdog <stefan.landwehr1000@gmail.com>
2026-05-22 20:33:18 +0200

    Add PathSelector for upcoming feature

--- main.py
+++ main.py
@@ -9,4 +9,13 @@ class CustomPath:
     Windows64Media =                StringVar(window, "D:/Games/MinecraftConsoles/Minecraft.Client/Windows64Media")
     Common_Media =                  StringVar(window, "D:/Games/MinecraftConsoles/Minecraft.Client/Common/Media/")
 
+class PathSelector:
+    def __init__(self, label_text):
+        Label(window, text=label_text).pack()
+
+test1 = PathSelector("hi")
+test2 = PathSelector("this")
+test3 = PathSelector("is")
+test4 = PathSelector("cool")
+
 window.mainloop()

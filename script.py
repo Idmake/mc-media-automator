9d99dbcb2aa478586f097ff84dc43d508596bbce
stefansdog <stefan.landwehr1000@gmail.com>
2026-05-26 16:53:22 +0200

    Fix wrong naming convention in script.py

--- script.py
+++ script.py
@@ -4,11 +4,11 @@ def run_script(CustomPath, PredefinedPath):
     import zipfile
     import tkinter.messagebox
 
-    def ExecuteCommand(args, shell):
+    def execute_command(args, shell):
         returnCode = subprocess.run(args=args, shell=shell).returncode
-        ErrorUponFaliure(returnCode)
+        error_upon_failure(returnCode)
 
-    def ErrorUponFaliure(returnCode):
+    def error_upon_failure(returnCode):
         if returnCode != 0:
             subprocess.run(f"echo:", shell=True)
             subprocess.run(f"echo Command failed to execute.", shell=True)
@@ -16,7 +16,7 @@ def run_script(CustomPath, PredefinedPath):
             tkinter.messagebox.showerror(message=f"An unknown error occurred; check the output for more information if possible.")
             raise ValueError(f"Error code {returnCode}")
         
-    def SetDirectory(path):
+    def set_directory(path):
         os.chdir(path=path)
 
 
@@ -25,32 +25,32 @@ def run_script(CustomPath, PredefinedPath):
     # erase "source file"
 
 
-    def BuildLanguages():
+    def build_languages():
         print("BUILDING LANGUAGES")
 
         # Create new languages.loc and strings.h files
-        SetDirectory(PredefinedPath.MCLCE_StringTable_Compiler)
-        ExecuteCommand(f"npm i", shell=True)
-        ExecuteCommand(f"node index.js build languages.loc --folder {CustomPath.Windows64Media_loc.get()}", shell=True)
+        set_directory(PredefinedPath.MCLCE_StringTable_Compiler)
+        execute_command(f"npm i", shell=True)
+        execute_command(f"node index.js build languages.loc --folder {CustomPath.Windows64Media_loc.get()}", shell=True)
 
-    def ReplaceStrings():
+    def replace_strings():
         print("REPLACING STRINGS FILE")
-        SetDirectory(PredefinedPath.MCLCE_StringTable_Compiler)
-        ExecuteCommand(f"xcopy strings.h \"{CustomPath.Windows64Media.get()}\" /Y /-I", shell=True)
+        set_directory(PredefinedPath.MCLCE_StringTable_Compiler)
+        execute_command(f"xcopy strings.h \"{CustomPath.Windows64Media.get()}\" /Y /-I", shell=True)
 
-    def GetLanguagesPath():
+    def get_languages_path():
         print("GETTING LANGUAGES PATH")
         print (f"{PredefinedPath.MCLCE_StringTable_Compiler + "/" + "languages.loc"}")
         return f"{PredefinedPath.MCLCE_StringTable_Compiler + "/" + "languages.loc"}" 
         
-    def ConvertArctoZip():
+    def convert_arc_to_zip():
         print("CONVERTING ARC TO ZIP")
-        SetDirectory(PredefinedPath.mc_arc_util_master)
-        ExecuteCommand(f"python arc2zip.py {FILE_PATH.MEDIAWINDOWS64_ARC}", shell=True)
+        set_directory(PredefinedPath.mc_arc_util_master)
+        execute_command(f"python arc2zip.py {FILE_PATH.MEDIAWINDOWS64_ARC}", shell=True)
 
-    def ModifyZip():
+    def modify_zip():
         print("MODIFIYING ZIP")
-        SetDirectory(PredefinedPath.mc_arc_util_master)
+        set_directory(PredefinedPath.mc_arc_util_master)
         tempName = "temp.zip"
         zipName = "arc.zip"    
 
@@ -59,7 +59,7 @@ def run_script(CustomPath, PredefinedPath):
         # We "copy" the languages.loc file which was made earlier.
         # This mimics the action of replacing a file but comes at the cost of being a fucking headache
         
-        ExecuteCommand(f"xcopy \"{zipName}\" \"{tempName}\" /Y /-I", shell=True)
+        execute_command(f"xcopy \"{zipName}\" \"{tempName}\" /Y /-I", shell=True)
         with zipfile.ZipFile(file=tempName, mode="r") as tempzip:
             with zipfile.ZipFile(file=zipName, mode="w", compression=zipfile.ZIP_DEFLATED) as zip:
                 # "Copy" the created languages.loc to the destination inside the zip.
@@ -68,30 +68,30 @@ def run_script(CustomPath, PredefinedPath):
                 for file in tempzip.filelist:
                     if (file.filename == "languages.loc"):
                         destination = "languages.loc"  
-                        sourcePath = GetLanguagesPath()                           
+                        sourcePath = get_languages_path()                           
                         zip.write(sourcePath, destination)
                     else:
                         zip.writestr(file, tempzip.read(file.filename))
                         
 
 
-    def ConvertZipToArc():
+    def convert_zip_to_arc():
         print("CONVERTING ZIP TO ARC")
-        SetDirectory(PredefinedPath.mc_arc_util_master)
-        ExecuteCommand(f"python zip2arc.py arc.zip", shell=True)
+        set_directory(PredefinedPath.mc_arc_util_master)
+        execute_command(f"python zip2arc.py arc.zip", shell=True)
 
-    def ReplaceMediaWindows64Arc():
+    def replace_mediawindows64_arc():
         print("REPLACING MEDIAWINDOWS64 ARC FILE")
-        SetDirectory(PredefinedPath.mc_arc_util_master)
+        set_directory(PredefinedPath.mc_arc_util_master)
         print("\n[WARNING] MediaWindows64.arc will be replaced with a new version, make sure to create a backup before you continue.")
-        ExecuteCommand(f"pause", shell=True)
-        ExecuteCommand(f"xcopy out.arc \"{FILE_PATH.MEDIAWINDOWS64_ARC}\" /Y /-I", shell=True)
+        execute_command(f"pause", shell=True)
+        execute_command(f"xcopy out.arc \"{FILE_PATH.MEDIAWINDOWS64_ARC}\" /Y /-I", shell=True)
 
 
 
-    BuildLanguages()
-    ReplaceStrings()
-    ConvertArctoZip()
-    ModifyZip()
-    ConvertZipToArc()
-    ReplaceMediaWindows64Arc()
+    build_languages()
+    replace_strings()
+    convert_arc_to_zip()
+    modify_zip()
+    convert_zip_to_arc()
+    replace_mediawindows64_arc()
